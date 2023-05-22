d600f0ba31fb2c97e3e92bc6853ec210d98ee42c
hudsam <app.hoedhya@gmail.com>
2023-05-22 20:00:28 +0700

    ➕ header.php

---
+++ template/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Identity -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="google" content="notranslate" />
+    <title><?php echo $title; ?> · PDDI Kota Malang</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+
+    <!-- Favicon -->
+    <link rel="icon" href="Logo KSR UB.png" sizes="75x75" type="image/png">
+</head>
